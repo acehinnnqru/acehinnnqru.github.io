f4d11ef8b9a18ea35fecb405c14b1e3ebfe407a2
acehinnnqru <acehinnnqru@gmail.com>
2022-10-28 01:44:21 +0800

    layout done

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
 // This starter template is using Vue 3 <script setup> SFCs
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
-import { ref } from "vue";
-import Markdown from "./components/Markdown.vue";
-
-const filepath = ref("/demo.md");
 </script>
 
 <template>
-  <div>
-    <p>
-      <router-link to="/">Home</router-link>
-      <router-link to="/about">About</router-link>
-    </p>
-    <router-view></router-view>
+  <div id="main">
+    <div id="main-container">
+      <div id="left-menu">
+        <div id="traffic-lights"></div>
+        <div id="catogries"></div>
+      </div>
+      <div></div>
+      <div id="right-container">
+        <div id="util-bar"></div>
+        <div id="content-container">
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,9 +1,9 @@
+<script setup lang="ts">
+        
+</script>
 <template>
     <div>
         <h1>Home</h1>
     </div>
 </template>
 
-<script setup>
-        
-</script>
