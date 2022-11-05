125f3efa88dee8d9c23f0f3ed06a36a48f0b8415
acehinnnqru <acehinnnqru@gmail.com>
2022-11-06 00:02:37 +0800

    update

---
+++ src/components/TrafficLights.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+</script>
+<template>
+  <div class="flex align-center m-16px ml-24px gap-8px ">
+    <div class="traffic-light bg-red-500 h-12px w-12px rounded-full"></div>
+    <div class="traffic-light bg-yellow-500 h-12px w-12px rounded-full"></div>
+    <div class="traffic-light bg-green-500 h-12px w-12px rounded-full"></div>
+  </div>
+</template>
+<style scoped>
+</style>
---
+++ src/components/UtilBar.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+    catogories: String
+});
+</script>
+<template>
+  <div>
+    <div>
+      <div class="h-15px w-15px">&#x3c</div>
+      <div class="h-15px w-15px">&#x3e</div>
+      <div class="h-20px w-min-15px">{{ catogories }}</div>
+    </div>
+    <div></div>
+  </div>
+</template>
+<style scoped>
+/* test */
+</style>
